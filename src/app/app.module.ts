e8e84e9ee817ddcca693e8389858994dca7e56b2
Cristian <cristianbcm1999@gmail.com>
2023-03-06 12:30:12 -0300

    about me finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { ExperienceSectionComponent } from './components/experience-section/experience-section.component';
+import { LoginComponent } from './components/login/login.component';
+import { PortfolioWebComponent } from './components/portfolio-web/portfolio-web.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    AboutMeComponent,
+    SkillsSectionComponent,
+    ExperienceSectionComponent,
+    LoginComponent,
+    PortfolioWebComponent
   ],
   imports: [
     BrowserModule
